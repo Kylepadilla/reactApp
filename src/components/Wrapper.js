7e802cad3c4b848565e835b79220674992b37700
kylepadilla <kylepadilla1@gmail.com>
2019-04-25 23:11:36 -0400

    updates

--- src/components/Wrapper.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import "./Wrapper.css"
-
-const Wrapper = props=>
- <div className = {props.errrr === 'true'? 'shake' : 'still'}>{props.images}</div>
-
-export default Wrapper;
