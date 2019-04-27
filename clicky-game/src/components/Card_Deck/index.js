088595232cc86ca4d93fa4a61250b56ecc5868bc
kylepadilla <kylepadilla1@gmail.com>
2019-04-26 20:10:41 -0400

    test

--- clicky-game/src/components/Card_Deck/index.js
+++ clicky-game/src/components/Card_Deck/index.js
@@ -1,16 +1,21 @@
-import React from "react";
-
-
+import React from 'react';
 
 function Carddeck(props) {
-  return (
-    <div className="card">
-      <div className="img-container">
-        <img className="img-thumbnail img-responsive" alt = "unavailable" src={props.images}/>
-        <span className="choose" onClick={()=>props.clickHandler(props.id)} >x</span>
-      </div>
-    </div>
-  );
+	console.log(props.images);
+	return (
+		<div className="card">
+			<div className="img-container">
+				<img
+					className="img-thumbnail img-responsive"
+					alt="unavailable"
+					src={process.env.PUBLIC_URL + props.images}
+				/>
+				<span className="choose" onClick={() => props.clickHandler(props.id)}>
+					x
+				</span>
+			</div>
+		</div>
+	);
 }
 
 export default Carddeck;
