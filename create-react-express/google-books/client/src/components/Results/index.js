b5babe07cadcca3d321fb49bfcb520541efe1488
kylepadilla <kylepadilla1@gmail.com>
2019-04-29 13:42:20 -0400

    updates

--- create-react-express/google-books/client/src/components/Results/index.js
+++ create-react-express/google-books/client/src/components/Results/index.js
@@ -1,16 +1,30 @@
 import React from "react"
-
+import "./styles.css"
 const Results = props => 
-<div className="list-group">
-  <a href="#" className="list-group-item list-group-item-action active">
+  <a href={props.infoLink} className="list-group-item list-group-item-action">
+    <h3 className="mb-1 title">{props.title}</h3>
+  <img src={props.image} className="mr-3"  id="pic" alt="Ooops"/>
+
+  <h5 className="mb-1">{props.subtitle}</h5>
+  <p className="mb-1">{props.description}</p>
     <div className="d-flex w-100 justify-content-between">
-      <h5 className="mb-1">{props.title}</h5>
-      <small>{props.year}</small>
+
+      <small className="text-muted">{props.rating}</small>
     </div>
-    <p className="mb-1">{props.text}</p>
-    <small>{props.author}.</small>
+
+    <ul className="list-group">
+  <li className="list-group-item">Publisher: {props.pub}</li>
+  <li className="list-group-item">Language: {props.lang}</li>
+  <li className="list-group-item">Language: {props.authors}</li>
+  <li className="list-group-item">Print Type: {props.printType}</li>
+  <li className="list-group-item">Page Count: {props.pageCount}</li>
+  <li className="list-group-item">Link: {props.infoLink}</li>
+  <li className="list-group-item">Saleability: {props.sale}</li>
+  </ul>
+  <button  type="button" className="btn btn-primary buttonC" onClick = {()=>props.save}>Save Book</button>
+
+   
   </a>
 
-</div>
 
 export default Results
--- create-react-express/google-books/client/src/pages/Books.js
+++ create-react-express/google-books/client/src/pages/Books.js
@@ -1,123 +1,138 @@
-import React, { Component } from "react";
-import DeleteBtn from "../components/DeleteBtn";
-import Jumbotron from "../components/Jumbotron";
-import Search from "../components/Search"
-import Results from "../components/Search"
-import API from "../utils/API";
-import { Link } from "react-router-dom";
-import { Col, Row, Container } from "../components/Grid";
-import { List, ListItem } from "../components/List";
-import { Input, TextArea, FormBtn } from "../components/Form";
-import axios from "axios";
+import React, { Component } from 'react';
+// import DeleteBtn from "../components/DeleteBtn";
+// import Jumbotron from "../components/Jumbotron";
+import Search from '../components/Search';
+import Results from '../components/Results';
+import API from '../utils/API';
+import { Link } from 'react-router-dom';
+// import { Col, Row, Container } from "../components/Grid";
+import { List, ListItem } from '../components/List';
+import { Input, FormBtn } from '../components/Form';
+import axios from 'axios';
 
 class Books extends Component {
-  state = {
-    books: [],
-    title: "",
-    author: "",
+	state = {
+		books: [],
+		title: '',
+		author: ''
+	};
+	// api key = AIzaSyAspRS4dGPy0chMnNbfZM8UmQrfEP_S7N0
+	handleInputChange = (event) => {
+		const { name, value } = event.target;
+		this.setState({
+			[name]: value
+		});
+	};
 
+	handleFormSubmit = (event) => {
+		event.preventDefault();
+
+		if (this.state.title) {
+			const titlesearch = this.state.title.replace(/([()[{*+.$^\\|?])/g, '\\$1');
+			const authsearch = this.state.author.replace(/([()[{*+.$^\\|?])/g, '\\$1');
+			// console.log('state: ' + this.state.books);
+			console.log(titlesearch + '  title-Search');
+			console.log(authsearch + '  auth-Search');
+
+			axios
+				.get(
+					'https://www.googleapis.com/books/v1/volumes?q=' +
+						titlesearch +
+						'+inauthor:' +
+						authsearch +
+						'&key=AIzaSyAspRS4dGPy0chMnNbfZM8UmQrfEP_S7N0'
+				)
+				.then((res) => {
+					const bookdata = res.data.items;
+          console.log(bookdata)
+					this.setState({
+						books: bookdata
+					});
+				})
+				.catch((err) => console.log(err));
+		}
   };
-
-  componentDidMount() {
-    this.loadBooks();
+  
+  clickHandler = event =>{
+      const book_Id = event.target.id
+      console.log(book_Id)
   }
 
-  loadBooks = () => {
-    API.getBooks()
-      .then(res =>{
-        console.log("load books: "+ res)
-        this.setState({ books: res.data, title: "", author: "" })}
-      )
-      .catch(err => console.log(err));
-  };
-
-  deleteBook = id => {
-    API.deleteBook(id)
-      .then(res => this.loadBooks())
-      .catch(err => console.log(err));
-  };
-
-  handleInputChange = event => {
-    const { name, value } = event.target;
-    this.setState({
-      [name]: value
-    });
-  };
-
-  handleFormSubmit = event => {
-    event.preventDefault();
-
-    const titlesearch = this.state.title
-    const authsearch = this.state.author
-    console.log(authsearch + "authsearch")
-    console.log(titlesearch)
-    if (titlesearch) {
-
-      axios.get("https://www.googleapis.com/books/v1/volumes?q="+ titlesearch +"+inauthor:"+ authsearch + "&key=AIzaSyAspRS4dGPy0chMnNbfZM8UmQrfEP_S7N0")
-        .then(res => {
-          console.log("res" +res) 
-          console.log("data  " + res) 
-
-          const bookdata = res
-
-         this.setState(
-           {
-             books: bookdata
-           }
-         )
-        }
-     )
-        .catch(err => console.log(err));
-    }
-  };
-
-  render() {
-    return (
-      // <Container fluid>
-  <div>
-            <Jumbotron>
-              <h1>(React) Google Books Search</h1>
-            </Jumbotron>
-            
-              <Search>
-
-              <Input
-                name="title"
-                value={this.state.title}
-                onChange={this.handleInputChange}
-                placeholder="Title (required)"
-                />    
-              <Input
-                name="author"
-                value={this.state.author}
-                onChange={this.handleInputChange}
-                placeholder="Author (optional)"
-                /> 
-                <FormBtn
-                disabled={!(this.state.author)}
-                onClick={this.handleFormSubmit}
-                />
-    
-                </Search>
-
-              
-               {this.state.books.map((book)=>(
- <Results
-   key={book.id}
-   id={book.id}
-   title= {book.title}
-   author= {book.author}
-   text= {book.synopsis}
-   year= {book.year}
-   />
-               )
-              )}
-
-</div>
-
-        
-    );
-  }
+	render() {
+		return (
+			<div>
+				<div style={{ height: 300, clear: 'both', paddingTop: 120, textAlign: 'center' }} className="jumbotron">
+					<h1>(React) Google Books Search</h1>
+				</div>
+				<div>
+					<Search>
+						<Input
+							name="title"
+							value={this.state.title}
+							onChange={this.handleInputChange}
+							placeholder="Title (required)"
+						/>
+						<Input
+							name="author"
+							value={this.state.author}
+							onChange={this.handleInputChange}
+							placeholder="Author (optional)"
+						/>
+						<FormBtn disabled={!this.state.title} onClick={this.handleFormSubmit} />
+					</Search>
+				</div>
+				<List>
+					{this.state.title ? (
+						this.state.books.map((book, i) => {
+							return (
+								<Results
+									key={book.id}
+                  id={book.id}
+                  image={book.volumeInfo.imageLinks.thumbnail}
+									title={book.volumeInfo.title}
+									subtitle={book.volumeInfo.subtitle}
+									authors={book.volumeInfo.authors[0]}
+									description={book.volumeInfo.description}
+                  rating={book.volumeInfo.maturityRating}
+                  printType={book.volumeInfo.printType}
+                  infoLink = {book.volumeInfo.infoLink}
+                  pageCount = {book.volumeInfo.pageCount}
+                  lag = {book.volumeInfo.language}
+                  sale = {book.saleInfo.saleability}
+                  pub = {book.volumeInfo.publisher}
+                  save={this.clickHandler}
+								/>
+							);
+						})
+					) : (
+						<h3>you can search for books above </h3>
+					)}
+				</List>
+			</div>
+		);
+	}
 }
+// 				<List>
+// 					{this.state.title ? (
+// 						this.state.books.map((book) => {
+// 							return (
+// 								<Results
+// 									key={book._id}
+// 									id={book._id}
+// 									title={book.volumeInfo.title}
+// 									authors={'book.volumeInfo.authors[0]'}
+// 									description={book.volumeInfo.description}
+// 									year={book.volumeInfo.date}
+// 								/>
+// 							);
+// 						})
+// 					) : (
+// 						<h3>you can search for books above </h3>
+// 					)}
+// 				</List>
+// 			</div>
+// 		);
+// 	}
+// }
 
 export default Books;
